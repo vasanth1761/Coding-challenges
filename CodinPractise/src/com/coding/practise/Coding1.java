ac3dea8800fa1c584654cf16f18a8eaf537632ac
Vasa3558 <Vasa3558@CSL0701.global.chainsys.com>
2024-04-15 17:42:11 +0530

    coding challenge day1

---
+++ CodinPractise/src/com/coding/practise/Coding1.java
@@ -0,0 +1,12 @@
+package com.coding.practise;
+
+public class Coding1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+
+	}
+
+}
---
+++ CodinPractise/src/com/coding/practise/Coding4.java
@@ -0,0 +1,24 @@
+package com.coding.practise;
+import java.util.*;
+
+public class Coding4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d=scan.nextDouble();
+        scan.nextLine();
+        String s=scan.nextLine();
+        
+        
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+
+	}
+
+}
